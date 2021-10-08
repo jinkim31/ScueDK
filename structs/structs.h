d049f86d0860449c8df7cbb4d015ca7aed9557e3
jinkim31 <jinkim5771@gmail.com>
2021-10-08 21:01:50 +0900

    revised struct descriptions

--- structs/structs.h
+++ structs/structs.h
@@ -8,7 +8,7 @@
  *
  * -- !!DISCLAIMERS!! --
  * 1. WRITE IN STANDARD C. IT WON'T BE COMPATIBLE WITH SLAVE SERIAL_LINE LIBRARY(WRITTEN IN C) OTHERWISE.
- * 2. DEFINE STRUCT FOR EACH SLAVE TYPE(motor driver, manipulator etc). DEFINE A MASTER STRUCT THAT HAS SLAVE STRUCTS DEDICATED TO EACH HARDWARE/SIMULATED SLAVE.
+ * 2. DEFINE STRUCT FOR EACH SLAVE TYPE(motor driver, manipulator etc.). DEFINE A "ASTER(WITH ITS NAME "MASTER") STRUCT THAT HAS SLAVE STRUCTS DEDICATED TO EACH HARDWARE/SIMULATED SLAVE.
  * 3. ENCAPSULATE EVERY VARIABLE INTO A STRUCT. 'STRAY' ONES MIGHT BE HARD TO SPECIFY THEIR ADDRESSES.
  */
 
@@ -28,25 +28,25 @@ namespace scue {
  * R : READ
  */
 
-typedef struct _MotorController
+typedef struct _MotorController         //flipper, track motor controller. hardware slave
 {
-	float trackTargetSpeed;         //S 회전속도 (rad/s)
-	float flipperATargetAngle;      //S 플리퍼A 각도 (rad)
-	float flipperBTargetAngle;      //S 플리퍼B 각도 (rad)
-    double encoderReading;          //R 누적 엔코더 각도. 오버플로우 없도록 주의 (rad)
+	float   trackTargetSpeed;           //S 회전속도 (rad/s)
+	float   flipperATargetAngle;        //S 플리퍼A 각도 (rad)
+	float   flipperBTargetAngle;        //S 플리퍼B 각도 (rad)
+    double  encoderReading;             //R 누적 엔코더 각도. 오버플로우 없도록 주의 (rad)
 }MotorController;
 
-typedef struct _Manipulator
+typedef struct _Manipulator             //manipulator. simulated slave
 {
-	float targetPosition[6];        //S 가장 아래부터 6축의 각도 목표값 (rad)
-	float gripperTargetCurrent;     //S 가장 아래부터 6축의 각속도 목표값 (rad/s)
-    float targetAcceleration[6];    //S 가장 아래부터 6축의 각가속도 목표값 (rad/s^2)
+	float   targetPosition[6];          //S 가장 아래축부터 6축의 각도 목표값 (rad)
+    float   targetAcceleration[6];      //S 가장 아래축부터 6축의 각가속도 목표값 (rad/s^2)
+    float   gripperTargetCurrent;       //S 그리퍼 전류제어 목표값(A)
 }Manipulator;
 
-typedef struct _MasterTweak
+typedef struct _MasterTweak             //option tweaks and init triggers for master board. simulated slave
 {
-	bool initManipulatorTrigger;    //매니퓰레이터 초기화 트리거
-	bool initFlipperTrigger;        //플리퍼 초기화 트리거
+	bool    initManipulatorTrigger;     //매니퓰레이터 초기화 트리거
+	bool    initFlipperTrigger;         //플리퍼 초기화 트리거
 }MasterTweak;
 
 /*
